297828661a4e600e1bc31f6a840e0094f793d951
Grace Ingledue <sging@live.unc.edu>
2020-06-28 18:02:22 -0400

    Initial commit

---
+++ blend.pde
@@ -0,0 +1,93 @@
+/* @pjs preload="flags/bi.png, flags/trans.png", "flags/genderqueer.png", "flags/lesbian.png",
+"flags/pan.png", "flags/gay.png", "flags/nonbinary.png", "flags/genderfluid.png", "flags/8stripe.png"; */
+
+
+ArrayList flags;
+ArrayList images;
+PImage img;
+
+void newFlags() {
+    flags = new ArrayList();
+}
+
+ArrayList getFlags() {
+    return flags;
+}
+
+String getDataURL() {
+    return document.getElementsByTagName("canvas")[0].toDataURL();
+}
+
+void setup() {
+    size(1400, 840);
+    flags = new ArrayList();
+    images = new ArrayList();
+    noLoop();
+}
+
+void draw() {
+    boolean firstRun = true;
+    img = createImage(1400, 840);
+    for (Flag flag : flags) {
+        if (firstRun) {
+            img = loadImage(flag.name);
+            firstRun = false;
+        }
+        else {
+            PImage blending = loadImage(flag.name);
+            img.blend(blending, 0, 0, 1400, 840, 0, 0, 1400, 840, flag.mode);
+        }
+    }
+    image(img, 0, 0);
+    /*
+    PImage img = loadImage("bi.jpg");
+    PImage img2 = loadImage("trans.jpg");
+    PImage img3 = loadImage("genderqueer.jpg");
+    PImage img4 = loadImage("lesbian.png");
+    img.blend(img2, 0, 0, 655, 395, 0, 0, 655, 395, MULTIPLY);
+    img.blend(img3, 0, 0, 655, 395, 0, 0, 655, 395, OVERLAY);
+    img.blend(img4, 0, 0, 655, 395, 300, 0, 655, 395, ADD);
+    image(img, 0, 0);
+    */
+}
+
+class Flag {
+    String name, mode;
+    Flag(String name, String mode) {
+        this.name = name;
+        this.mode = getMode(mode);
+    }
+
+    PConstants getMode(mode) {
+        switch(mode) {
+            case "BLEND":
+                return BLEND;
+            case "ADD":
+                return ADD;
+            case "SUBTRACT":
+                return SUBTRACT;
+            case "DARKEST":
+                return DARKEST;
+            case "LIGHTEST":
+                return LIGHTEST;
+            case "DIFFERENCE":
+                return DIFFERENCE;
+            case "EXCLUSION":
+                return EXCLUSION;
+            case "MULTIPLY":
+                return MULTIPLY;
+            case "SCREEN":
+                return SCREEN;
+            case "OVERLAY":
+                return OVERLAY;
+            case "HARD_LIGHT":
+                return HARD_LIGHT;
+            case "SOFT_LIGHT":
+                return SOFT_LIGHT;
+            case "DODGE":
+                return DODGE;
+            case "BURN":
+                return BURN;
+        }
+    }
+}
---
+++ script.js
@@ -0,0 +1,114 @@
+function draw_thing(flags) {
+    clearInterval(tId);
+
+    var canvas = Processing.getInstanceById("main");
+    canvas.newFlags();
+    var canvasFlags = canvas.getFlags();
+    for (var item in flags) {
+        canvasFlags.add(new canvas.Flag(flags[item].name, flags[item].mode));
+    }
+    canvas.redraw();
+    document.getElementById("flag").setAttribute("src", canvas.getDataURL());
+}
+
+function toTitleCase(str) {
+    return str.replace(
+        /\w\S*/g,
+        function(txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    );
+}
+
+$(function() {
+    const modeList = ["DISABLED", "BASE", "ADD", "SUBTRACT", "DARKEST", "LIGHTEST", "DIFFERENCE", "EXCLUSION", "MULTIPLY", "SCREEN", "OVERLAY", "HARD_LIGHT", "SOFT_LIGHT", "DODGE", "BURN"]
+    const flagList = {
+        "Lesbian": "lesbian.png",
+        "Gay": "gay.png",
+        "Bi": "bi.png",
+        "Trans": "trans.png",
+        "Genderqueer": "genderqueer.png",
+        "Pan": "pan.png",
+        "Nonbinary": "nonbinary.png",
+        "Genderfluid": "genderfluid.png",
+        "8-Stripe": "8stripe.png",
+    }
+    for (var item in flagList) {
+        element = `<div><p>${item}</p><img src='flags/${flagList[item]}'>`
+            + `<input id='${item}Name' name='${item}' value='flags/${flagList[item]}'></input>`
+            + `<select id='${item}Mode' class='mode ${item}' name='${item}Mode'>`;
+        for (var mode in modeList) {
+            element += `<option value='${modeList[mode]}'>` +
+                `${toTitleCase(modeList[mode].replace('_', ' '))}</option>`;
+        }
+        element += `</select><select id='${item}Order 'class='order ${item}' name='${item}Order'>` +
+            "<option value='None'>None</option>"
+        for (i=1; i<Object.keys(flagList).length; i++) {
+            order = i;
+            if (i==0) {
+                order = "Base";
+            }
+            element += `<option value='${order}'>${order}</option>`;
+        }
+        element += "</select></div>";
+        $("form").append(element);
+    }
+
+    var modes = $(".mode");
+    var selects = $("select");
+
+    function onUpdate() {
+        $.each(selects, function(index, value) {
+            $(value).prop("disabled", false);
+        });
+        var flags = []
+        var formArray = $("form").serializeArray();
+        var newArray = [];
+        flags = [];
+        for (i=0; i<formArray.length/3; i++) {
+            newArray.push({file: formArray[i*3+0].value,
+                mode: formArray[i*3+1].value,
+                order: formArray[i*3+2].value});
+        }
+        console.log(newArray);
+        for (i=0; i<newArray.length; i++) {
+            for (var flag in newArray) {
+                if ((i==0 && newArray[flag].mode=="BASE") ||
+                    (i==newArray[flag].order)) {
+                        flags.push({name: newArray[flag].file,
+                            mode: newArray[flag].mode});
+                        break;
+                }
+            }
+        }
+        draw_thing(flags);
+        $.each(modes, function(index, value) {
+            checkMode(value);
+        });
+        /*
+        $.each(formArray, function(index, value) {
+            if (value.value == "Base") {
+                flags.push
+            }
+        });*/
+    }
+
+    function checkMode(element) {
+        val = $(element).val();
+        elemClass = $(element).attr("class").replace('mode ', '');
+        if (val == "DISABLED" || val == "BASE") {
+            $orderElem = $(".order." + elemClass);
+            $orderElem.prop("disabled", true);
+            $orderElem.val("None");
+        }
+    }
+
+    $.each(modes, function(index, value) {
+        value.addEventListener("input", function() { checkMode(value) });
+    });
+    $.each(selects, function(index, value) {
+        value.addEventListener("input", onUpdate);
+    });
+});
+
+tId = setInterval(draw_thing, 1000);
